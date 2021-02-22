1b16de700deed83a145d5a066069c15a4672389d
Samuel Stalschus <samuel5stalschus@gmail.com>
2021-02-21 23:07:05 -0300

    update

--- src/server.js
+++ src/server.js
@@ -2,33 +2,16 @@ const express = require("express")
 const ejs = require("ejs")
 const path = require("path")
 const puppeteer = require("puppeteer")
-const { response } = require("express")
 const app = express()
 
-const passengers = [
-  {
-    name: "Samuel",
-    flightNumber: 7859,
-    time: "09:00",
-  },
-  {
-    name: "Samara",
-    flightNumber: 7859,
-    time: "09:00",
-  },
-  {
-    name: "Davi",
-    flightNumber: 7859,
-    time: "09:00",
-  },
-]
-
-app.get("/pdf", async (req, res) => {
-  //Com headless true o puppeteer não abre o navegador na tela
-  const browser = await puppeteer.launch()
+app.get("/:link", async (req, res) => {
+  const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
+  let link = req.params.link
 
-  await page.goto("http://localhost:3000/", {
+  link = "https://" + link
+
+  await page.goto(link, {
     waitUntil: "networkidle0",
   })
 
@@ -50,17 +33,4 @@ app.get("/pdf", async (req, res) => {
   return res.send(pdf)
 })
 
-app.get("/", (req, res) => {
-  ejs.renderFile(
-    path.join(__dirname, "print.ejs"),
-    { passengers },
-    (error, html) => {
-      if (error) {
-        return response.send("Erro na leitura do arquivo")
-      }
-      return res.send(html)
-    }
-  )
-})
-
-app.listen(3000)
+app.listen(3003)
